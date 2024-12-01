0dc87486b9d2d6a5354481556ce7b8bd1f12a277
Henriette Lehtonen <Henriette.Lehtonen@student.lab.fi>
2024-12-01 20:45:40 +0200

    kirjautumine formin kautta toimii

--- Louna-db/src/index.js
+++ Louna-db/src/index.js
@@ -6,6 +6,7 @@ import { menuRouter } from "./routes/menu-router.js";
 import { userRouter } from "./routes/user-routes.js";
 import authRouter from "./routes/auth-router.js";
 import { notFoundHandler, errorHandler } from "./middlewares/error-handler.js";
+//
 const hostname = "127.0.0.1";
 const app = express();
 const port = 3000;
@@ -13,6 +14,7 @@ const port = 3000;
 app.set("view engine", "pug");
 app.set("views", "src/views");
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // To parse application/x-www-form-urlencoded
 app.use(express.static("public"));
 app.use("/uploads", express.static("uploads"));
 
